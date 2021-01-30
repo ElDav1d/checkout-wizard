5db14d63bf708d38f409f62ea52c263c9abb2eb4
David Vivó Palanca <david.vivo@nexum.com>
2021-01-30 16:32:22 +0100

    Store user data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    userData: {
+      name: "",
+      lastName: "",
+    }
+  },
+  getters: {
+    getUserData: state => {
+      return state.userData;
+    }
+  },
+  mutations: {
+    SAVE_USER_DATA(state, userData) {
+      state.userData = userData;
+    }
+  },
+  actions: {
+    saveUserData({ commit }, userData) {
+      commit("SAVE_USER_DATA", userData);
+    }
+  },
+  plugins: [createPersistedState()]
 });
--- src/views/StepInput.vue
+++ src/views/StepInput.vue
@@ -1,5 +1,83 @@
 <template>
   <main>
     <h1>StepInput</h1>
+    <form>
+      <fieldset>
+        <label for="name">Nombre</label>
+        <input
+          id="name"
+          type="text"
+          required
+          v-model="name"
+          @blur="inputBlurHandler"
+        />
+        <div v-if="!nameIsValid">required</div>
+      </fieldset>
+      <fieldset>
+        <label for="lastName">Apellido</label>
+        <input
+          id="lastName"
+          type="text"
+          required
+          v-model="lastName"
+          @blur="inputBlurHandler"
+        />
+        <div v-if="!lastNameIsValid">required</div>
+      </fieldset>
+      <input type="submit" @click="submitDataHandler" :disabled="!validated" />
+    </form>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      nameIsValid: true,
+      lastName: "",
+      lastNameIsValid: true,
+    };
+  },
+  methods: {
+    setValue(key, value) {
+      this[key] = value;
+    },
+    inputBlurHandler(event) {
+      const keyString = `${event.target.id}IsValid`;
+      if (!event.target.value) {
+        this.setValue(keyString, false);
+      } else {
+        this.setValue(keyString, true);
+      }
+    },
+    getUserData() {
+      return {
+        name: this.name,
+        lastName: this.lastName,
+      };
+    },
+    saveUserData(data) {
+      this.$store.dispatch("saveUserData", data);
+    },
+    resetUserData() {
+      this.name = "";
+      this.lastName = "";
+    },
+    goToStep(step) {
+      this.$router.push(step);
+    },
+    submitDataHandler(event) {
+      event.preventDefault();
+      this.saveUserData(this.getUserData());
+      this.resetUserData();
+      this.goToStep("pago-del-producto");
+    },
+  },
+  computed: {
+    validated() {
+      return !!this.name && !!this.lastName;
+    },
+  },
+};
+</script>
