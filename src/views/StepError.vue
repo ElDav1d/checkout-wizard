9f999ecc69597bcabdd364fba40c7432bcb129fc
David Vivó Palanca <david.vivo@nexum.com>
2021-01-31 20:04:02 +0100

    Personalize error page

--- src/views/StepError.vue
+++ src/views/StepError.vue
@@ -1,6 +1,10 @@
 <template>
   <main>
-    <h1>StepError</h1>
+    <h1>{{ title }}</h1>
+    <h2>{{ personalMessage }}.</h2>
+    <p>
+      {{ copy }}
+    </p>
     <router-link :to="paymentPath">Volver</router-link>
   </main>
 </template>
@@ -11,8 +15,17 @@ import config from "../config.js";
 export default {
   data() {
     return {
+      name: this.$store.getters.getUserData.name,
       paymentPath: `/${config.PAYMENT_PATH}`,
+      title: "¡Ooooooops!",
+      copy:
+        "No te preocupes, aún podemos continuar con el pago desde donde lo dejaste.",
     };
+  },
+  computed: {
+    personalMessage() {
+      return `Vaya, ${this.name}...¡parece que ha habido un problema!`;
+    },
   },
 };
 </script>
