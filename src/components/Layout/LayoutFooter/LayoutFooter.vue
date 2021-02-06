15d39db36f630ff641614b4eb700a337c6e03ac3
David Vivó Palanca <david.vivo@nexum.com>
2021-02-06 21:50:56 +0100

    Style footer

--- src/components/Layout/LayoutFooter/LayoutFooter.vue
+++ src/components/Layout/LayoutFooter/LayoutFooter.vue
@@ -1,11 +1,24 @@
 <template>
-  <footer>
-    FOOTER
+  <footer class="LayoutFooter">
+    <LinkedLogo class="LayoutFooter_LogoLink" />
+    <p class="LayoutFooter_Copy">{{ copy }}</p>
   </footer>
 </template>
 
 <script>
+import LinkedLogo from "../../../components/LinkedLogo/LinkedLogo.vue";
 export default {
   name: "layout-footer",
+  data() {
+    return {
+      copy: "2020 © We Are Marketing",
+    };
+  },
+  components: {
+    LinkedLogo,
+  },
 };
 </script>
+<style lang="scss">
+@import "./LayoutFooter.scss";
+</style>
---
+++ src/components/LinkedLogo/LinkedLogo.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-link class="LinkedLogo" :to="homePath">
+    <img
+      class="LinkedLogo_Image"
+      src="./Logo.jpg"
+      :title="title"
+      :alt="title"
+    />
+  </router-link>
+</template>
+<script>
+import config from "../../config.js";
+export default {
+  name: "linked-logo",
+  data() {
+    return {
+      homePath: config.HOME_PATH,
+      title: "Logo de We Are Marketing",
+    };
+  },
+};
+</script>
+<style lang="scss">
+@import "./LinkedLogo.scss";
+</style>
